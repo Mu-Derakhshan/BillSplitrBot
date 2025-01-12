05c992db50c46f7838a5a2aa7564e5c188d25c67
houdini <erfanehteshamykia@gmail.com>
2025-01-13 02:23:05 +0330

    bugfix

--- app/handlers.py
+++ app/handlers.py
@@ -7,7 +7,6 @@ def add_bill_handler(data):
     for entity in data["message"]["entities"]:
         if entity["type"] == "mention":
             username = data["message"]["text"][entity["offset"]:entity["offset"]+entity["length"]]
-            print(getChat(username))
             user_id = get_user_id(username)
             user_ids.append(user_id)
         elif entity["type"] == "text_mention":
