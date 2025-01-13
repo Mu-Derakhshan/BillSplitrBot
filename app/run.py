5a6f58f0becd6b623bec93ce0e4855fdf75cb2ac
houdini <erfanehteshamykia@gmail.com>
2025-01-13 21:38:14 +0330

    bug fixing

--- app/run.py
+++ app/run.py
@@ -6,29 +6,13 @@ app = create_app()
 if __name__ == "__main__":
     setMyCommands(
         [
-            {
-                "command": "register",
-                "description": "register yourself in this group",
-            },
-            {
-                "command": "add_bill",
-                "description": "add your bill",
-            },
-            {
-                "command": "pay",
-                "description": "use this to pay your debts",
-            },
-            {
-                "command": "summary",
-                "description": "summary of all bills in the group",
-            },
+            {"command": "register", "description": "register yourself in this group"},
+            {"command": "add_bill", "description": "add your bill"},
+            {"command": "pay", "description": "use this to pay your debts"},
+            {"command": "summary", "description": "summary of all bills in the group"},
             {
                 "command": "my_debts",
                 "description": "see the summary of your bills only",
-            },
-            {
-                "command": "reset",
-                "description": "reset and delete the history of all expenses and bills history",
             },
         ]
     )
