da13879218de103d70e441fce99ea618f8df7f22
houdini <erfanehteshamykia@gmail.com>
2025-01-13 03:40:18 +0330

    back to async

--- app/handlers.py
+++ app/handlers.py
@@ -9,10 +9,15 @@ def add_bill_handler(data):
     for entity in data["message"]["entities"]:
         if entity["type"] == "mention":
             username = data["message"]["text"][entity["offset"]:entity["offset"]+entity["length"]]
+
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
             client = get_client()
             with client:
                 user_id = client.loop.run_until_complete(get_user_id(username))
                 user_ids.append(user_id)
+                
         elif entity["type"] == "text_mention":
             user_id = entity["user"]["id"]
             user_ids.append(user_id)
