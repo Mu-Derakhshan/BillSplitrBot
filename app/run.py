022cbfd474118b3562c96874bcee34931fbe9713
houdini <erfanehteshamykia@gmail.com>
2025-01-13 21:50:19 +0330

    bug fixing

--- app/run.py
+++ app/run.py
@@ -19,6 +19,19 @@ if __name__ == "__main__":
     )
     setMyCommands(
         [
+            {"command": "register", "description": "register yourself in this group"},
+            {"command": "add_bill", "description": "add your bill"},
+            {"command": "pay", "description": "use this to pay your debts"},
+            {"command": "summary", "description": "summary of all bills in the group"},
+            {
+                "command": "my_debts",
+                "description": "see the summary of your bills only",
+            },
+        ],
+        scope={"type": "all_chat_administrators"}
+    )
+    setMyCommands(
+        [
             {"command": "reset", "description": "remove all expenses and bills paid and unpaid use carefully"},
         ],
         scope={"type": "all_chat_administrators"}
