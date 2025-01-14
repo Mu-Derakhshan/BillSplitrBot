6f458257db49b9fbbb1342a96e86fc1fcf3b337b
houdini <erfanehteshamykia@gmail.com>
2025-01-15 00:39:16 +0330

    escaping !

--- app/helpers.py
+++ app/helpers.py
@@ -49,7 +49,7 @@ def extract_amount(data):
 
 
 def escape_markdown_v2(text):
-    special_chars = r"#._"
+    special_chars = r"#._!"
     for char in special_chars:
         text = text.replace(char, f"\\{char}")
     return text
