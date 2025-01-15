e5e9dc89aafa058817d1ed1c5862ae0d1b6caf6a
houdini <erfanehteshamykia@gmail.com>
2025-01-15 12:31:13 +0330

    created a db with username and password and now app tries to connect to it

--- app/config.py
+++ app/config.py
@@ -1,4 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")
+host = os.getenv("MONGO_HOST")
+port = os.getenv("MONGO_PORT", 27017)
+database_name = os.getenv("MONGO_DATABASE")
+
 class Config:
     DEBUG = True
     SECRET_KEY = "your_secret_key"
-    MONGO_URI = "mongodb://localhost:27017/your_database_name"  # MongoDB URI
+    MONGO_URI = f"mongodb://{username}:{password}@{host}:{port}/{database_name}?authSource=admin"
