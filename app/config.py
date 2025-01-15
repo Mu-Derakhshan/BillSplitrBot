8ab209871cea1968c9632150b850ff963c4a76ef
houdini <erfanehteshamykia@gmail.com>
2025-01-15 12:40:58 +0330

    bugfix

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -9,7 +10,10 @@ host = os.getenv("MONGO_HOST")
 port = os.getenv("MONGO_PORT", 27017)
 database_name = os.getenv("MONGO_DATABASE")
 
+escaped_username = quote_plus(username)
+escaped_password = quote_plus(password)
+
 class Config:
     DEBUG = True
     SECRET_KEY = "your_secret_key"
-    MONGO_URI = f"mongodb://{username}:{password}@{host}:{port}/{database_name}?authSource=admin"
+    MONGO_URI = f"mongodb://{escaped_username}:{escaped_password}@{host}:{port}/{database_name}?authSource=admin"
